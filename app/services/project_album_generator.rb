3a78eda365d0dc272895b1f253781fcb8b00a915
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2025-10-14 07:59:13 -0400

    Create project_album_generator.rb

---
+++ app/services/project_album_generator.rb
@@ -0,0 +1,16 @@
+class ProjectAlbumGenerator
+
+    def self.call(project)
+        albums = Album.where.not(project: project)
+
+        project.project_albums.create(album: album)
+
+        used_album_ids = project.project_albums.pluck(:album_id)
+        album = Album.where.not(id: used_album_ids).order("RANDOM()").first
+
+        return unless album #if no album, then we have generated all the albums
+
+        project.project_albums.create!(album: album)
+    end
+
+end
